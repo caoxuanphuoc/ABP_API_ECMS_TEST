96144ae7a90e0c5938b3f424614616d8fdf68d69
nguyenduchieu112001 <nguyenduchieu112001@gmail.com>
2023-09-21 16:25:38 +0700

    #2 Add: Create UserClassDto

---
+++ 7.3.0/aspnet-core/src/EMS.Application/UserClasses/Dto/UserClassDto.cs
@@ -0,0 +1,17 @@
+﻿using Abp.Application.Services.Dto;
+using Abp.AutoMapper;
+using EMS.Authorization.UserClasses;
+using EMS.Users.Dto;
+using System;
+
+namespace EMS.UserClasses.Dto
+{
+    [AutoMapFrom(typeof(UserClass))]
+    public class UserClassDto : EntityDto<long>
+    {
+        public UserDto User { get; set; }
+        public DateTime OffTimes { get; set; }
+        public DateTime DateStart { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
