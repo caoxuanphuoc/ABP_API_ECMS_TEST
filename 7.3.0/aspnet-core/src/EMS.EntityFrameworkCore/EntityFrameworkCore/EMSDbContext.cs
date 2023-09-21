0876e8282d38ff777c57836cf2c77fcc4a3c5e9f
nguyenduchieu112001 <nguyenduchieu112001@gmail.com>
2023-09-21 12:00:53 +0700

    #1 Add: Create DbContext

--- 7.3.0/aspnet-core/src/EMS.EntityFrameworkCore/EntityFrameworkCore/EMSDbContext.cs
+++ 7.3.0/aspnet-core/src/EMS.EntityFrameworkCore/EntityFrameworkCore/EMSDbContext.cs
@@ -1,15 +1,28 @@
-﻿using Microsoft.EntityFrameworkCore;
-using Abp.Zero.EntityFrameworkCore;
+﻿using Abp.Zero.EntityFrameworkCore;
+using EMS.Authorization.Classes;
+using EMS.Authorization.Courses;
 using EMS.Authorization.Roles;
+using EMS.Authorization.Schedules;
+using EMS.Authorization.TrackingClasses;
+using EMS.Authorization.TuitionFees;
+using EMS.Authorization.UserClasses;
 using EMS.Authorization.Users;
+using EMS.Authorization.WorkShifts;
 using EMS.MultiTenancy;
+using Microsoft.EntityFrameworkCore;
 
 namespace EMS.EntityFrameworkCore
 {
     public class EMSDbContext : AbpZeroDbContext<Tenant, Role, User, EMSDbContext>
     {
         /* Define a DbSet for each entity of the application */
-        
+        public DbSet<UserClass> UserClasses { get; set; }
+        public DbSet<TuitionFee> TuitionFees { get; set; }
+        public DbSet<TrackingClass> TrackingClasses { get; set; }
+        public DbSet<Class> Classes { get; set; }
+        public DbSet<Schedule> Schedules { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<WorkShift> WorkShifts { get; set; }
         public EMSDbContext(DbContextOptions<EMSDbContext> options)
             : base(options)
         {
