1c98ee7f3809fe8015c174812627fa0e2db52c3a
nguyenduchieu112001 <nguyenduchieu112001@gmail.com>
2023-09-21 11:58:05 +0700

    #1 Add : Initial ScheduleDb

---
+++ 7.3.0/aspnet-core/src/EMS.Core/Authorization/Schedules/Schedule.cs
@@ -0,0 +1,20 @@
+﻿using Abp.Domain.Entities.Auditing;
+using EMS.Authorization.Classes;
+using EMS.Authorization.WorkShifts;
+using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EMS.Authorization.Schedules
+{
+    [Table("AbpSchedule")]
+    public class Schedule : FullAuditedEntity<long>
+    {
+        public DateTime Date { get; set; }
+        [ForeignKey("Class")]
+        public long ClassId { get; set; }
+        public Class Class { get; set; }
+        [ForeignKey("WorkShift")]
+        public long WorkShiftId { get; set; }
+        public WorkShift WorkShift { get; set; }
+    }
+}
