bf33c59f94fe5765b6b2e0758b18b0738ea35d41
nguyenduchieu112001 <nguyenduchieu112001@gmail.com>
2023-09-21 11:57:39 +0700

    #1 Add: Initial CourseDb

---
+++ 7.3.0/aspnet-core/src/EMS.Core/Authorization/Courses/Course.cs
@@ -0,0 +1,16 @@
+﻿using Abp.Domain.Entities.Auditing;
+using EMS.Authorization.Classes;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EMS.Authorization.Courses
+{
+    [Table("AbpCourse")]
+    public class Course : FullAuditedEntity<long>
+    {
+        public string CourseName { get; set; }
+        public long CourseFee { get; set; }
+        public long Quantity { get; set; }
+        public ICollection<Class> Classes { get; set; }
+    }
+}
