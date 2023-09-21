07e3c11c5f8c8fa7ff82be99cb60cfd5375ea1a4
nguyenduchieu112001 <nguyenduchieu112001@gmail.com>
2023-09-21 11:58:25 +0700

    #1 Add: Intial TrackingClassDb

---
+++ 7.3.0/aspnet-core/src/EMS.Core/Authorization/TrackingClasses/TrackingClass.cs
@@ -0,0 +1,17 @@
+﻿using Abp.Domain.Entities.Auditing;
+using EMS.Authorization.UserClasses;
+using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EMS.Authorization.TrackingClasses
+{
+    [Table("AbpTrackingClass")]
+    public class TrackingClass : FullAuditedEntity<long>
+    {
+        public DateTime Date { get; set; }
+        public DateTime CheckInTime { get; set; }
+        [ForeignKey("UserClass")]
+        public long StudentId { get; set; }
+        public UserClass UserClass { get; set; }
+    }
+}
