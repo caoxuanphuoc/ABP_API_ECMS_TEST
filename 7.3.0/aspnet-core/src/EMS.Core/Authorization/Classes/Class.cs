e39effb9a82d5b5876e71636dd4d82b8b2c7c1d6
nguyenduchieu112001 <nguyenduchieu112001@gmail.com>
2023-09-21 11:57:20 +0700

    #1 Add : Initial ClassDb

---
+++ 7.3.0/aspnet-core/src/EMS.Core/Authorization/Classes/Class.cs
@@ -0,0 +1,31 @@
+﻿using Abp.Domain.Entities.Auditing;
+using EMS.Authorization.Courses;
+using EMS.Authorization.Schedules;
+using EMS.Authorization.UserClasses;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EMS.Authorization.Classes
+{
+    [Table("AbpClass")]
+    public class Class : FullAuditedEntity<long>
+    {
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public long LimitStudent { get; set; }
+        public long CurrentStudent { get; set; }
+        public DateTime LessionTimes { get; set; }
+        public DateTime CycleTimes { get; set; }
+        public bool IsActive { get; set; }
+        [ForeignKey("UserClass")]
+        public long TeacherId { get; set; }
+        public UserClass UserClass { get; set; }
+        [ForeignKey("Course")]
+        public long CourseId { get; set; }
+        public Course Course { get; set; }
+        public ICollection<Schedule> Schedules { get; set; }
+
+
+    }
+}
