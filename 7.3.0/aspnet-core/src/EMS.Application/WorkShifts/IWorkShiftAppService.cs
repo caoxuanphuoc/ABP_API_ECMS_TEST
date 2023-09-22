b240eed3b20c825575774bf5829f97ad89fe38fd
nguyenduchieu112001 <nguyenduchieu112001@gmail.com>
2023-09-22 10:52:22 +0700

    #2 Add: CRUD WorkShiftAppService

---
+++ 7.3.0/aspnet-core/src/EMS.Application/WorkShifts/IWorkShiftAppService.cs
@@ -0,0 +1,9 @@
+﻿using Abp.Application.Services;
+using EMS.WorkShifts.Dto;
+
+namespace EMS.WorkShifts
+{
+    public interface IWorkShiftAppService : IAsyncCrudAppService<WorkShiftDto, long, PagedWorkShiftResultRequestDto, CreateOrUpdateWorkShiftDto, CreateOrUpdateWorkShiftDto>
+    {
+    }
+}
