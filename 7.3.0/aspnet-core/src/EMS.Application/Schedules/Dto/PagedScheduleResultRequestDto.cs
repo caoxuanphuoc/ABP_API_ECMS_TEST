bbc977fb49538e9c4cd45e8ed833915030238058
nguyenduchieu112001 <nguyenduchieu112001@gmail.com>
2023-09-22 11:28:55 +0700

    #2 Add: Create dto for research by keyword

---
+++ 7.3.0/aspnet-core/src/EMS.Application/Schedules/Dto/PagedScheduleResultRequestDto.cs
@@ -0,0 +1,9 @@
+﻿using Abp.Application.Services.Dto;
+
+namespace EMS.Schedules.Dto
+{
+    public class PagedScheduleResultRequestDto : PagedAndSortedResultRequestDto
+    {
+        public string Keyword { get; set; }
+    }
+}
