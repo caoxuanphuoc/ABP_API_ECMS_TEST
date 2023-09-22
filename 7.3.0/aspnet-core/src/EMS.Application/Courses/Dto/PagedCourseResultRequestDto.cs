251c797483a0b896360a9649ece9bd6de839db59
nguyenduchieu112001 <nguyenduchieu112001@gmail.com>
2023-09-22 10:49:37 +0700

    #2 Add: Create dto for research by keyword

---
+++ 7.3.0/aspnet-core/src/EMS.Application/Courses/Dto/PagedCourseResultRequestDto.cs
@@ -0,0 +1,9 @@
+﻿using Abp.Application.Services.Dto;
+
+namespace EMS.Courses.Dto
+{
+    public class PagedCourseResultRequestDto : PagedAndSortedResultRequestDto
+    {
+        public string Keyword { get; set; }
+    }
+}
