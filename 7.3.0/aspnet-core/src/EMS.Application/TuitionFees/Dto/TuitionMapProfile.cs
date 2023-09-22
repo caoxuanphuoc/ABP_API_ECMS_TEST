2ff27d08f1ef545f08d4d5cba593e58ccb3e4f30
nguyenduchieu112001 <nguyenduchieu112001@gmail.com>
2023-09-22 09:01:03 +0700

    #2 Add: Create Map Profile to map value

---
+++ 7.3.0/aspnet-core/src/EMS.Application/TuitionFees/Dto/TuitionMapProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using EMS.Authorization.TuitionFees;
+using EMS.Authorization.UserClasses;
+using EMS.UserClasses.Dto;
+
+namespace EMS.TuitionFees.Dto
+{
+    public class TuitionMapProfile : Profile
+    {
+        public TuitionMapProfile()
+        {
+            CreateMap<UserClassDto, UserClass>().ReverseMap();
+            CreateMap<TuitionFeeDto, TuitionFee>().ReverseMap();
+
+            CreateMap<TuitionFee, TuitionFeeDto>()
+                .ForMember(dest => dest.Student, opt => opt.MapFrom(src => src.UserClass));
+        }
+    }
+}
