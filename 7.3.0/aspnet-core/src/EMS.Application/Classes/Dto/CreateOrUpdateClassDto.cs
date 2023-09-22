d0d99c1a850976bc799601005d9d804008cd0d71
nguyenduchieu112001 <nguyenduchieu112001@gmail.com>
2023-09-22 10:46:25 +0700

    #2 Add: Create dto for create new or update class

---
+++ 7.3.0/aspnet-core/src/EMS.Application/Classes/Dto/CreateOrUpdateClassDto.cs
@@ -0,0 +1,31 @@
+﻿using Abp.Application.Services.Dto;
+using Abp.AutoMapper;
+using EMS.Authorization.Classes;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace EMS.Classes.Dto
+{
+    [AutoMapTo(typeof(Class))]
+    public class CreateOrUpdateClassDto : EntityDto<long>
+    {
+        [Required]
+        public long CourseId { get; set; }
+        [Required]
+        public long TeacherId { get; set; }
+        [Required]
+        public DateTime StartDate { get; set; }
+        [Required]
+        public DateTime EndDate { get; set; }
+        [Required]
+        public long LimitStudent { get; set; }
+        [Required]
+        public long CurrentStudent { get; set; }
+        [Required]
+        public DateTime LessionTimes { get; set; }
+        [Required]
+        public DateTime CycleTimes { get; set; }
+        [Required]
+        public bool IsActive { get; set; }
+    }
+}
