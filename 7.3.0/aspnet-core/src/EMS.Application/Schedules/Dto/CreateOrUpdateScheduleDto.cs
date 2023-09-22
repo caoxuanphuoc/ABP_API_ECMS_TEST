786dbbf0563ebf58da99c668cfa57220c7a719f8
nguyenduchieu112001 <nguyenduchieu112001@gmail.com>
2023-09-22 11:28:35 +0700

    #2 Add: Create dto for create new or update Schedule

---
+++ 7.3.0/aspnet-core/src/EMS.Application/Schedules/Dto/CreateOrUpdateScheduleDto.cs
@@ -0,0 +1,19 @@
+﻿using Abp.Application.Services.Dto;
+using Abp.AutoMapper;
+using EMS.Authorization.Schedules;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace EMS.Schedules.Dto
+{
+    [AutoMapTo(typeof(Schedule))]
+    public class CreateOrUpdateScheduleDto : EntityDto<long>
+    {
+        [Required]
+        public long ClassId { get; set; }
+        [Required]
+        public long WorkShiftId { get; set; }
+        [Required]
+        public DateTime Date { get; set; }
+    }
+}
