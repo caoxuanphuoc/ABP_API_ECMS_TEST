ae72ae45fee289d16b6f83b8bf737c7910590116
nguyenduchieu112001 <nguyenduchieu112001@gmail.com>
2023-09-27 14:44:15 +0700

    #1 Update: Change relationship

--- 7.3.0/aspnet-core/src/EMS.Core/Authorization/Classes/Class.cs
+++ 7.3.0/aspnet-core/src/EMS.Core/Authorization/Classes/Class.cs
@@ -15,17 +15,12 @@ namespace EMS.Authorization.Classes
         public DateTime EndDate { get; set; }
         public long LimitStudent { get; set; }
         public long CurrentStudent { get; set; }
-        public DateTime LessionTimes { get; set; }
-        public DateTime CycleTimes { get; set; }
+        public int LessionTimes { get; set; } // Số buổi học
         public bool IsActive { get; set; }
-        [ForeignKey("UserClass")]
-        public long TeacherId { get; set; }
-        public UserClass UserClass { get; set; }
         [ForeignKey("Course")]
         public long CourseId { get; set; }
         public Course Course { get; set; }
         public ICollection<Schedule> Schedules { get; set; }
-
-
+        public ICollection<UserClass> UserClasses { get; set; }
     }
 }
