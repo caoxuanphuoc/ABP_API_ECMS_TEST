8eb08326234bed43a58b3b722798ec66d3baae12
nguyenduchieu112001 <nguyenduchieu112001@gmail.com>
2023-09-29 17:07:44 +0700

    Add: add CRUD PositionAppService

---
+++ 7.3.0/aspnet-core/src/EMS.Application/Positions/IPositionAppService.cs
@@ -0,0 +1,9 @@
+﻿using Abp.Application.Services;
+using EMS.Positions.dto;
+
+namespace EMS.Positions
+{
+    public interface IPositionAppService : IAsyncCrudAppService<PositionDto, long, PagedPositionResultRequestDto, CreateOrUpdatePositionDto, CreateOrUpdatePositionDto>
+    {
+    }
+}
