0ee14f40a5475e7b4a03e6c4bcb8e8296d5e1301
nguyenduchieu112001 <nguyenduchieu112001@gmail.com>
2023-10-02 10:21:22 +0700

    Add: CRUD method TeacherAppService

---
+++ 7.3.0/aspnet-core/src/EMS.Application/Teachers/Dto/TeacherDto.cs
@@ -0,0 +1,18 @@
+﻿using Abp.Application.Services.Dto;
+using Abp.AutoMapper;
+using EMS.Authorization.Teachers;
+using EMS.Users.Dto;
+using System;
+
+namespace EMS.Teachers.Dto
+{
+    [AutoMapFrom(typeof(Teacher))]
+    public class TeacherDto : EntityDto<long>
+    {
+        public UserDto User { get; set; }
+        public string SchoolName { get; set; }
+        public string Certificate { get; set; }
+        public long Wage { get; set; }
+        public DateTime StartTime { get; set; }
+    }
+}
