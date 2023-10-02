a4f91db342998408522b69faa702ce96f412d1c5
nguyenduchieu112001 <nguyenduchieu112001@gmail.com>
2023-10-02 09:36:14 +0700

    Update: Change dto of ClassAppService

---
+++ 7.3.0/aspnet-core/src/EMS.Application/Classes/Dto/UpdateClassDto.cs
@@ -0,0 +1,29 @@
+﻿using Abp.Application.Services.Dto;
+using Abp.AutoMapper;
+using EMS.Authorization.Classes;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace EMS.Classes.Dto
+{
+    [AutoMapTo(typeof(Class))]
+    public class UpdateClassDto : EntityDto<long>
+    {
+        [Required]
+        public string Code { get; set; }
+        [Required]
+        public long CourseId { get; set; }
+        [Required]
+        public DateTime StartDate { get; set; }
+        [Required]
+        public DateTime EndDate { get; set; }
+        [Required]
+        public long LimitStudent { get; set; }
+        [Required]
+        public long CurrentStudent { get; set; }
+        [Required]
+        public int LessionTimes { get; set; }
+        [Required]
+        public bool IsActive { get; set; }
+    }
+}
