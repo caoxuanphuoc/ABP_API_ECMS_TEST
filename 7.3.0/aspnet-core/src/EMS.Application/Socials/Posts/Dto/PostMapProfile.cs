0801be894ed3a0a15157e3b7ed3ebeeabcd19eb9
nguyenduchieu112001 <nguyenduchieu112001@gmail.com>
2023-11-27 16:25:37 +0700

    Add Profile to map value of PostDto

---
+++ 7.3.0/aspnet-core/src/EMS.Application/Socials/Posts/Dto/PostMapProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using EMS.Social.Posts;
+
+namespace EMS.Socials.Posts.Dto
+{
+    public class PostMapProfile : Profile
+    {
+        public PostMapProfile()
+        {
+            CreateMap<Post, PostDto>()
+                .ForMember(dest => dest.FromUserId, opt => opt.MapFrom(src => src.CreatorUserId))
+                .ForMember(dest => dest.TimeToPost, opt => opt.MapFrom(
+                            src => (src.LastModificationTime != null) ? src.LastModificationTime : src.CreationTime));
+        }
+    }
+}
