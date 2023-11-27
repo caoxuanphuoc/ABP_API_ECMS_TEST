e49ebf8356137738de25d8fa973ebc9a0f1c047f
nguyenduchieu112001 <nguyenduchieu112001@gmail.com>
2023-11-27 16:13:25 +0700

    Add Dto to Upload File

---
+++ 7.3.0/aspnet-core/src/EMS.Application/Users/Dto/FileDto.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace EMS.Users.Dto
+{
+    public class FileDto
+    {
+        public IFormFile file { get; set; }
+        public string DriveUrl { get; set; }
+    }
+}
