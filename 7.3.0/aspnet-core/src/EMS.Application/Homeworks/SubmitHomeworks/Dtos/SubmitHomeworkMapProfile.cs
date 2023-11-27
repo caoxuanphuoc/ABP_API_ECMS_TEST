1916ab3513ccb28bf1c9f32eb688f31ee28f47f8
nguyenduchieu112001 <nguyenduchieu112001@gmail.com>
2023-11-27 16:32:38 +0700

    Add Profile to map value of SubmitHomework

---
+++ 7.3.0/aspnet-core/src/EMS.Application/Homeworks/SubmitHomeworks/Dtos/SubmitHomeworkMapProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+
+namespace EMS.Homeworks.SubmitHomeworks.Dtos
+{
+    public class SubmitHomeworkMapProfile : Profile
+    {
+        public SubmitHomeworkMapProfile()
+        {
+            CreateMap<SubmitHomeWork, SubmitHomeworkDto>()
+            .ForMember(dest => dest.FromUserId, opt => opt.MapFrom(src => src.CreatorUserId))
+            .ForMember(dest => dest.TimeToSubmit, opt => opt.MapFrom(
+                src => (src.LastModificationTime != null) ? src.LastModificationTime : src.CreationTime));
+        }
+    }
+}
