c5377035c3d2b09ebf4e02c884d47a62cf331571
nguyenduchieu112001 <nguyenduchieu112001@gmail.com>
2023-11-29 14:48:30 +0700

    Add HashCode function and Fix GetAll Function

--- 7.3.0/aspnet-core/src/EMS.Application/Schedules/IScheduleAppService.cs
+++ 7.3.0/aspnet-core/src/EMS.Application/Schedules/IScheduleAppService.cs
@@ -1,10 +1,11 @@
 ﻿using Abp.Application.Services;
 using EMS.Schedules.Dto;
+using System.Threading.Tasks;
 
 namespace EMS.Schedules
 {
     public interface IScheduleAppService : IAsyncCrudAppService<ScheduleDto, long, PagedScheduleResultRequestDto, CreateScheduleDto, UpdateScheduleDto>
     {
-        //Task<PagedResultDto<ScheduleDto>> GetAllWithClassIdFilter(PagedScheduleResultRequestDto input, long classId);
+        Task<string> HashSchedule(long id);
     }
 }
