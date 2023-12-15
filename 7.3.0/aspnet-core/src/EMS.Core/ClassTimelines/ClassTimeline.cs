816108052e026519fe052bde203eca33e97c2727
nguyenduchieu112001 <nguyenduchieu112001@gmail.com>
2023-12-15 11:56:29 +0700

    Add: add ClassTimeline model

---
+++ 7.3.0/aspnet-core/src/EMS.Core/ClassTimelines/ClassTimeline.cs
@@ -0,0 +1,21 @@
+﻿using Abp.Domain.Entities.Auditing;
+using EMS.Authorization.Classes;
+using EMS.Authorization.Schedules;
+using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EMS.ClassTimelines
+{
+    [Table("AbpClassTimeline")]
+    public class ClassTimeline : FullAuditedEntity<long>
+    {
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        [ForeignKey("Class")]
+        public long ClassId { get; set; }
+        public Class Class { get; set; }
+        [ForeignKey("Schedule")]
+        public long ScheduleId { get; set; }
+        public Schedule Schedule { get; set; }
+    }
+}
